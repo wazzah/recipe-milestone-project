94cb8dad47c41f360456f6b7c0b27116b0301439
Warren Howes <warren.howes@hotmail.co.uk>
2020-06-26 10:54:30 +0000

    added enviroment variables

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.eviron.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 @app.route('/')
 def hello():
