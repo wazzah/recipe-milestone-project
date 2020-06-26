57dfae92341e5b01ed202b064f9bf5b62d92878b
Warren Howes <warren.howes@hotmail.co.uk>
2020-06-26 11:56:31 +0000

    added templates

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
+from os import path
 if os.path.exists("env.py"):
-    import env
+    import env 
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.eviron.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+
+app.config["MONGO_DBNAME"] = 'recipe_manager'
+app.config['MONGO_URI'] = os.environ.get("MONGO_URI")
+
+mongo =PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World Again'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)    
+            port=int(os.environ.get('PORT')),
+            debug=True)
